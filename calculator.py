3014b21da5273a6b56ba64906ee3a565a81a6d74
pikajuz <pikajuz@gmail.com>
2026-09-10 16:40:09 +0700

    add calculator

---
+++ calculator.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from tkinter import ttk, messagebox
+
+GUI = Tk()
+GUI.title('Basic Git & GitHub by Uncle Engineer')
+GUI.geometry('500x700')
+
+L = Label(GUI,text='Pika')
+L.pack()
+
+L = Label(GUI,text='your username')
+L.pack()
+
+L = Label(GUI,text='your username')
+L.pack()
+
+GUI.mainloop()
